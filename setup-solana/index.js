473de592ef6c2a526ccf99d27f43f8a46f484cf5
OpeOginni <brightoginni123@gmail.com>
2024-04-03 11:55:43 +0100

    Setup Solana LocalNet Folder

--- setup-solana/index.js
+++
@@ -1,7 +0,0 @@
-var http = require("http"),
-  httpProxy = require("http-proxy");
-
-const GITPOD_RUN_URL = "";
-
-httpProxy.createProxyServer({ target: GITPOD_RUN_URL, changeOrigin: true }).listen(8899);
-console.log("listening on: 8899");
